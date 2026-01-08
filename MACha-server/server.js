5beac1dee93d206232c4e36a47e06498f4871387
Nhat Tan <trantanyo@gmail.com>
2026-01-09 01:25:12 +0700

    add logic server

--- MACha-server/server.js
+++ MACha-server/server.js
@@ -33,7 +33,11 @@ connectRedis().catch(err => {
     console.error('❌ Redis connection failed:', err.message);
 });
 
-const allowedOrigin = process.env.ORIGIN_URL?.replace(/\/$/, '') || 'http://localhost:3000';
+if (process.env.NODE_ENV === 'development') {
+    const allowedOrigin = process.env.ORIGIN_URL?.replace(/\/$/, '') || 'http://localhost:3000';
+} else {
+    const allowedOrigin = process.env.ORIGIN_PROD?.replace(/\/$/, '') || 'http://localhost:3000';
+}
 
 app.use(cors({
     origin: (origin, callback) => {
